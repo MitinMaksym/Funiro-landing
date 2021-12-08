ea71691d6502f34fc50bc24705af4d16441e5aff
MitinMaksym <mitinmaksym@gmail.com>
2021-12-09 00:09:15 +0200

    feat: styles for search form

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,26 +1,30 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
- import { isMobile,removeClasses } from "./functions.js";
+import { isMobile, removeClasses } from "./functions.js";
 
-window.onload = function(){
-    document.addEventListener('click', documentActions)
+window.onload = function () {
+  document.addEventListener("click", documentActions);
 
-    function documentActions(e){
-        const targetEl = e.target
+  function documentActions(e) {
+    const targetEl = e.target;
+    const searchInput = document.querySelector(".search-form");
 
-        if (window.innerWidth > 768 && isMobile.any()) {
-            if (targetEl.classList.contains('menu__arrow')) {
-                const parentItem = targetEl.closest('.menu__item')
-                parentItem.classList.toggle('_hover')
-            }
-        if(!targetEl.closest('.menu__item')) {
-            const itemsWithHover = document.querySelectorAll("._hover")
-            if (itemsWithHover) {
-                removeClasses(itemsWithHover, '_hover')
-            }
-        } 
-            
+    if (window.innerWidth > 768 && isMobile.any()) {
+      if (targetEl.classList.contains("menu__arrow")) {
+        const parentItem = targetEl.closest(".menu__item");
+        parentItem.classList.toggle("_hover");
+      }
+      if (!targetEl.closest(".menu__item")) {
+        const itemsWithHover = document.querySelectorAll("._hover");
+        if (itemsWithHover) {
+          removeClasses(itemsWithHover, "_hover");
         }
+      }
+
+      if (targetEl.classList.contains("search-form__icon")) {
+        searchInput.classList.toggle("_active");
+      } else if (!targetEl.closest(".search-form")) {
+        searchInput.classList.remove("_active");
+      }
     }
-}
-
-
+  }
+};
