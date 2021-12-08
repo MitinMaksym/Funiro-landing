8db4eb93e621c75a6df09993933c6ef2de983ce3
Roman Sobol <roman.sobol@praetoriusbelykh.de>
2021-12-08 18:27:43 +0200

    feat: add script for displaying menu items

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,26 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
-// import { isMobile } from "./functions.js";
+ import { isMobile,removeClasses } from "./functions.js";
+
+window.onload = function(){
+    document.addEventListener('click', documentActions)
+
+    function documentActions(e){
+        const targetEl = e.target
+
+        if (window.innerWidth > 768 && isMobile.any()) {
+            if (targetEl.classList.contains('menu__arrow')) {
+                const parentItem = targetEl.closest('.menu__item')
+                parentItem.classList.toggle('_hover')
+            }
+        if(!targetEl.closest('.menu__item')) {
+            const itemsWithHover = document.querySelectorAll("._hover")
+            if (itemsWithHover) {
+                removeClasses(itemsWithHover, '_hover')
+            }
+        } 
+            
+        }
+    }
+}
 
 
